99d236b7a88acda4f6155fc6249bca9037b39a82
Don Welch <dwelch@nait.ca>
2026-01-13 15:56:48 -0700

    exploring basic c# grammar

---
+++ Top-Level-Program-Solution/Top-Level-Program/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//this is an output statement to display to
+//  your console screen.
+//syntax:  static-classname.methodname(argument)
+
+Console.WriteLine("Hello, World!");
+
+//variables
+//C# is a strongly-typed language
+//once a variable has been declare of a specific
+//   datatype, that variable WILL be ONLY that datatype
+//Special: var variable
+//          this obtains its datatype went it if first
+//          encountered in your program according to
+//          the data type of the value
+
+//syntax datatype variablename [= value];
+//       datatype variablename [, variable2name, ...];
+
+//defualt for numerics 0
+
+int rawGrade;
+int weight, maxGrade;
+
+
+
+
