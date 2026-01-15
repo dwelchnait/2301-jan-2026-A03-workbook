327d16da5697d62cf4d78b90df61ab90cd4539eb
Don Welch <dwelch@nait.ca>
2026-01-15 16:47:43 -0700

    complete exploration of fundamental coding structures.

--- Top-Level-Program-Solution/Top-Level-Program/Program.cs
+++ Top-Level-Program-Solution/Top-Level-Program/Program.cs
@@ -20,8 +20,8 @@ Console.WriteLine("Hello, World!");
 
 //defualt for numerics 0
 
-int rawGrade;
-int weight, maxGrade;
+double rawGrade;
+int weight, maxGrade = 0;
 double weightMark = 0.0;
 
 //decimal literals need to be identified by having an m at the end
@@ -48,26 +48,120 @@ DateTime theDate = DateTime.Today;
 string inputValue;
 
 //Write() does NOT move to the next line
-Console.Write("Enter the course id:/t");
+Console.Write("Enter the course id:\t");
 inputValue = Console.ReadLine();
 courseID = inputValue;
 Console.Write("Enter course mark weight:\t");
 inputValue = Console.ReadLine();
 weight = int.Parse(inputValue);
-Console.Write("Enter max mark :\t");
-inputValue = Console.ReadLine();
-maxGrade = int.Parse(inputValue);
+
+//example of looping to obtain a valid input value
+bool flag = false;
+while (flag != true)
+{
+    Console.Write("Enter max mark :\t");
+    inputValue = Console.ReadLine();
+    //TryParse attempts to convert your value
+    //  if successful, it places the converted value in the out variable
+    //                 and returns a true value
+    //  if unsuccessful, no conversion is done, the out variable receives no value
+    //                  and returns a false value
+    if (int.TryParse(inputValue, out maxGrade) )
+    {
+        if (maxGrade <= 0)
+        {
+            Console.WriteLine($"\n\tMax grade cannot be a negative number\n");
+        }
+        else
+            flag=true;
+    }
+    else
+    {
+        Console.WriteLine($"\n\tMax grade is not a numeric\n");
+    }
+}
+
 Console.Write("Enter raw score :\t");
 inputValue = Console.ReadLine();
-rawGrade = int.Parse(inputValue);
+rawGrade = double.Parse(inputValue);
 
-//Calculations
-weightMark = rawGrade / maxGrade * weight;
+//when using a structure that could require a group of statement
+//  to be execute you will need to place the group in a coding block {....}
+//if you structure will only require one statement
+//  to be executed then the coding block { } are optional
 
-//output
-//string concatenation
-Console.WriteLine($"\nYour mark in {courseID} is {weightMark}");
+// if ()
+//    statement 1; ONLY statement 1 is consider to be part of the if structure
+//    statement 2:
 
+// if ()
+//  {
+//    statement 1; 
+//    statement 2:
+//  }
+
+//condition statement
+//a) if (condition) [{] true path [}]
+//b) if (condition) [{] true path [}] else [{] false path[}]
+//c) if (condition) [{] true path [}] else if (condition) [{] true path [}] .... else [{] false path[}]
+//d) result value = (condition) ? true value : false value;
+// as long a the code for the true value or false value resolves to a 
+//      single value, your statement is value
+//  in the following example the "nested" t-condition will resolve
+//      to either the true value or the false value which will be
+//      the false value for the outer t-condition
+// result value = (condition) ? true value : (condition) ? true value :  false value;
+
+//logic operators
+// and &&
+// or ||
+// not !
+// bit-and &
+// bit-or |
+
+if (rawGrade < 0 || rawGrade > maxGrade)
+{
+    Console.WriteLine($"Your raw grade of {rawGrade} is invalid. " +
+        $"You need a value between 0 and {maxGrade}.");
+    //using a return or exit or break or continue command
+    //within a coding structure will be considered unstructured code
+    //AND
+    //result in marks lost in evaluation
+    //return;
+}
+else
+{
+
+    //Calculations
+    //the result of your calculation is dependent on your
+    //  variable data type
+    //problem here is the calculation variables are integers
+    //          therefore the calculation uses the rules of integer arth.
+    //Solutions:
+    //a) change the datatype of your variables
+    //      may cause problems elsewhere in your code, REMEMBER to retest your program
+
+    //b) if one of the variables' datatype is different then others
+    //      and allows for increased numeric representation (ie integer to double)
+    //      then C# will attempt to do the calculation at the greater
+    //      numeric representation
+
+    //c) use a type-cast on your field(s)
+    //      a type-cast is a temporary internal changing of how to handle
+    //      the variable's data
+    weightMark = rawGrade / (double)maxGrade * weight;
+
+    //output
+    //string concatenation
+    //formating numerics
+    Console.WriteLine("\nYour mark in {0} is {1:##0.0}",courseID,weightMark);
+
+    Console.WriteLine("\nYour mark in " +
+                       courseID + " is " +
+                       Math.Round(weightMark,1));
+
+    Console.WriteLine($"\nYour mark in {courseID} is {weightMark.ToString("#0.0")}");
+}
 
 
 
