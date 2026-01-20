172f0dea5b2e44caa5fcf93cb7e9110a13b1416d
Don Welch <dwelch@nait.ca>
2026-01-20 16:49:24 -0700

    create OOPs solution and project, create Person class, explore namespace, class access level and data members

---
+++ OOPsProgramming/LearningObjects/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+//a namespace is use to indicate organization with our code
+//all items within the same namespace can be referenced without
+//  addition using clauses
+
+namespace LearningObjects
+{
+    //the access level of your class definition
+    //public
+    //  can be use by any code that has access to the namespace
+    //private
+    //  ?????
+    //internal 
+    //  can be use by any other code within the same structure
+    //      as the class definition
+    public class Person
+    {
+        //data members
+        //fields, attributes, data members
+        //hold a piece of data
+        //data is valuable
+        //securing access by making them private
+        //access and modification will be done via
+        // other components of the class
+
+        private string Name;
+        private int Age;
+        private decimal Wage;
+
+        //properties
+
+        //methods
+        //constructor
+
+
+        //behaviours (aka a method)
+    }
+}
---
+++ OOPsProgramming/LearningObjects/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LearningObjects;
+
+Console.WriteLine("Hello, World!");
+
+//reference a class within the LearningObjects namespace
+Person Me;
