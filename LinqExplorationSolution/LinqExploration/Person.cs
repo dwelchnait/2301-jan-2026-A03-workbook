daec2b081c4b8e3e0b67b7885d0b0667370d0e7a
Don Welch <dwelch@nait.ca>
2026-02-09 15:03:38 -0700

    intro to collections and Linq mehtods

---
+++ LinqExplorationSolution/LinqExploration/Person.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+
+//a namespace is use to indicate organization with our code
+//all items within the same namespace can be referenced without
+//  addition using clauses
+
+namespace LearningObjects
+{
+    //the access level of your class definition
+    //public
+    //  can be use by any code that has access to the namespace
+    //private
+    //  ?????
+    //internal 
+    //  can be use by any other code within the same structure
+    //      as the class definition
+    public class Person
+    {
+        //data members
+        //fields, attributes, data members
+        //hold a piece of data
+        //data is valuable
+        //securing access by making them private
+        //access and modification will be done via
+        // other components of the class
+
+        //typically, you will find that C# coders will place an underscore
+        //  at the start of the data member names
+        //this allows for the property to be called the same name but without
+        //  the underscore
+        //this helps in associating the data member to the supporting property
+        //  in one's mind
+
+        private string _Name;
+        private int _Age;
+
+
+        //properties
+
+        //are associated with a single piece of data.
+        //Properties can be implemented by:
+        //  a) fully implemented property
+        //  b) auto implemented property
+
+        //A property does not need to store data
+        //  this type of property is referred to as a read-only
+        //  this property typically uses existing data values
+        //      within the instance to return a computed value
+        //A property must have a get, the set is optional
+
+        //fully implemented properties usually has additional logic
+        //  to execute for control over the data: such as validation
+        //fully implemented properties will have a declared data
+        //  member to store the data into
+
+        //auto implemented properties do not have additional logic
+        //Auto implemented properties do not have a declared
+        //  data member instead the o/s will create on the property's
+        //  behalf a storage area that is accessible ONLY by the property
+
+        ///<summary>
+        ///Property: Name
+        ///datatype: string
+        ///validation: there must be a character in the string
+        ///a property will always have a getter (accessor)
+        ///a property may or may not have a setter (mutator)
+        /// no mutator the property is consider "read-only" and is
+        ///         usually returning a computed field
+        /// has a mutator, the property will at some point save the data
+        ///     to storage
+        /// the mutator may be public (default) or private
+        ///     public: accessible by outside users of the class
+        ///     private: accessible ONLY within the class, usually
+        ///                 via the constructor or a method
+        /// !!!!! a property DOES NOT have ANY declared incoming parameters !!!!!!
+        /// </summary>
+        ///
+
+        //fully implemented propety version for Name is accociated with the
+        //  data member _Name
+        //public string Name
+        //{
+        //    //accessor (getter)
+        //    //returns the string associated with this property
+        //    get { return _Name; }
+
+        //    //mutator (setter)
+        //    //the incoming data value is accessed within the property
+        //    //  using the keyword: value
+        //    //the incoming value to the instance's property  will be stored
+        //    //  in the data member _Name.
+
+        //    //it is within the set that the validation of the data
+        //    //  is done to determine if the data is acceptable
+        //    //if all processing of the string is done via the property
+        //    //  it will ensure that good data is within the associated string
+        //    set
+        //    {
+        //        if (string.IsNullOrWhiteSpace(value))
+        //        {
+        //            //classes do not output directly to the program user
+        //            //classes throw Exceptions that are handled by the program and displayed to the 
+        //            //  human user
+        //            //there are many types of exceptions within C#
+        //            // general, catchall, exception: Exception
+        //            // specific exceptions:
+        //            //      ArgumentNullException (used when missing a value)
+        //            //      ArgumentException ( used when the value supplied is incorrect)
+        //            throw new ArgumentNullException("Name", "Name cannot be empty or just blanks");
+        //        }
+        //        else
+        //        {
+        //            //it is a very good practice to remove leading and trailing spaces on strings
+        //            //  so that only the required and important characters are stored.
+        //            //to do this sanitization use .Trim()
+        //            //this does not remove embedded blanks
+        //            _Name = value.Trim();
+        //        }
+
+        //    }
+        //}
+
+        public string Name
+        {
+          
+            get { return _Name; }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException("Name","Name cannot be missing or blank");
+                _Name = value.Trim();
+
+            }
+        }
+        //auto implemented property version for Name
+        //public string Name { get; set; }
+
+        ///<summary>
+        ///Property: Age
+        ///validation: the value must be 0 or greater
+        ///datatype: int
+        ///</summary>
+        ///
+
+        public int Age
+        {
+            get { return _Age; }
+            set
+            {
+                if (value < 0)
+                    // {
+                    // a throw cause execution to stop and exit the property or method
+                    throw new ArgumentException($"The age {value} is invalid. Age must be 0 or greater", "Age");
+                // }
+                // else
+                // {
+                _Age = value;
+                // }
+            }
+        } //eop end of property
+
+        //create an auto implemented property for Wage data
+        //the o/s will create the storage area for saving the data within the class instance (object)
+
+        //one option on the setter is to make it private
+        //if private:
+        //  changes to this data will ONLY be done via a constructor or a method
+        //  one will NOT be able to directly change the data via the property
+        //  DO NOT make your getter private
+
+        //if you validation is NOT in the property, EVERY time you alter the data
+        //  you NEED to consider if validation is required.
+        //if validation is required, that validation MUST be placed in your code
+        //  WHEREVER the alternation is done
+
+        public decimal Wage { get; private set; }
+
+
+        //methods
+        //constructor
+
+        //constructor ensure that when an class instance (object) is created, the contents
+        //  of the object will be as "expected" (in a known state)
+
+        //your class does not technically need a coded constructor
+        //if you code a constructor for your class you are responsible for coding ALL constructors
+        //if you do not code a constructor then the system will assign the software datatype defaults
+        //  to your variables (data members/auto-implemented properties)
+
+        //syntax: accesslevel constructorname([list of parameters]) { .... }
+        //NOTE: NO return datatype
+        //      the constructorname MUST be the class name
+
+        //Default
+        //simulates the "system defaults"
+        public Person()
+        {
+            //if there is no code within this constructor, the actions for setting
+            //  your internal fields will be using the system defaults for the datatype
+
+            //optionally!!!!!
+            // you could assign values to your initial fields within this constructor typically
+            //      using literal values
+            //Why?
+            // your internal fields may have validation attached to the data for the field
+            // this validation is usually within the property
+            // you would wish to have valid data values for your internal fields
+
+            //BEST PRACTICE: always wherever possible save and access your instance data
+            //                  via the properties, specially, when the property contains validation
+            Name = "Unknown";
+
+            //Age and Wage
+            //the default values is 0 (fine)
+            //HOWEVER, if you wish you could code the initial value
+            Age = 0;
+            Wage = 0.0m; //m is need as the literal needs to be a decimal not double
+        }
+
+        //Greedy
+        //this is the constructor typically used to assign values to a instance at the time of
+        //    creation
+        //the list of parameters may or may not contain default parameter values
+        //if you have assigned default parameter values then those parameters MUST be at the end of
+        //  the parameter list
+        //in this example wage is a default parameter (it has an assigned value if the value
+        //  is not included on the coded constructor in the user program
+        public Person(string name, int age, decimal wage = 0.00m)
+        {
+           
+            Name = name;
+            Age = age;
+
+            //if wage was NOT on the call statement then the value of the parameter
+            //  defaults to 0.00
+            //if wage was supplied on the call statement then the supplied value is the
+            //  contents of the parameter
+
+            //one could add validation to the constructor, especially if the property has
+            //  a private set OR 
+            //  the property is an auto-implemented property that has restrictions
+            //example: Wage
+            //         must be 0 or greater
+            if (wage < 0)
+                throw new ArgumentException($"The wage {wage} is invalid. Wage must be 0 or greater", "Wage");
+            Wage = wage;
+        }
+
+
+        //Methods (aka Behaviours)
+
+        //syntax of a general method
+        // accesslevel [override] returndatatype methodname ([list of parameters])
+        // {
+        //     coding block
+        //  }
+
+        // realize that to use a method or any item within your instance you must have the instance
+        //  if you have the instance then you have the data within the instance ALREADY
+        //  THEREFORE, if the data is within the instance you DO NOT need to pass the data into your method
+
+        //we would like to dump the data within the instance to "somewhere"
+        public override string ToString()
+        {
+            //each instance has a ToString() method as part of its construction
+            //you can overide the default coding of the supplied method
+            //to use your own code, create the method with the special keyword "override"
+            //in this example the delimiter for the value will be a ; due to the fact
+            //  I wish to use a comma , in the Wage display
+            return $"{Name};{Age};{Wage.ToString("#,##0.00")}";
+        }
+
+        public decimal CalculateBonus(decimal yearsofservice)
+        {
+            //Notice: Wage is not passed in, as it is already known within the instance
+            // yearsofservice is NOT stored within the instance THERFORE it MUST be a parameter
+            return Wage * yearsofservice;
+        }
+
+        //this method will allow the alteration of the Wage data
+        //if the setter on Wage was NOT private then this method would NOT be needed
+        public void ChangeWage(decimal wage)
+        {
+            if (wage < 0)
+                throw new ArgumentException($"The wage {wage} is invalid. Wage must be 0 or greater", "Wage");
+            Wage = wage;
+        }
+    }
+}
---
+++ LinqExplorationSolution/LinqExploration/Program.cs
@@ -0,0 +1,176 @@
+﻿using LearningObjects; //have knowledge of Person class
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\n\tWorld of Exploration on Collections!\n");
+
+//create a collection in C#
+//a collection could be any structure that holds more than one occurrence of an item
+//  a one time
+
+//array[] will hold a fixed number items
+//List<T> will an unknown number of items
+//          <T> indicates the datatype of the items being held
+
+List<Person> workers = null; //a variable capable of holding a List instance of datatype Person
+
+//create the List<T> instance
+workers = new List<Person>();
+
+//you can do the previous two statements together
+//List<Person> workers = new List<Person>();
+
+//at this point the list exists but it is empty
+Console.WriteLine($"The list of workers is empty. It exists but the worker count is {workers.Count}.");
+
+//create an instance of your Person class
+Person person = new Person("Don", 70, 101500.00m);
+
+//add the instance of person to the collection
+workers.Add(person);
+
+//shorten the statement by usde the .Add(new class(....))
+workers.Add(new Person("Pat", 32, 45780.00m));
+workers.Add(new Person("Evan", 32, 77777.00m));
+workers.Add(new Person("Terry", 20, 56678.00m));
+workers.Add(new Person("Sam", 60, 89901.00m));
+
+//Remember, a list is flexible
+//List can hold zero, 1 or more items of your <T> datatype
+Console.WriteLine($"\nWorker list exists and the worker count is {workers.Count}.");
+
+//pass a list<T> to a method as an argument
+DisplayCollection(workers);
+
+// --- STEP 1: Filter (Where(predicate)) -------------------------------
+// the common form of a predicate
+//  instanceplacehoder => instanceplaceholderAttribute operator value
+//worker is an instance of a class. Accessing a component in the class uses the dot operator followed
+//      by a property, a public data member or a method
+//what is returned from the .Where() are instances of the workers collection that pass
+//      the predicate condition
+//the datatype var is set during run time and will not change
+//Linq will return either a IEnumerable or IQueryable dataset
+//Within C# programming the dataset will probably be IEnumerable
+//you can convert the default IEnumerable<T> to a List<T> using the method .ToList();
+
+//can the predicate condition have logically operators (and/or): yes
+//  instanceplacehoder => (instanceplaceholderAttribute operator value &&
+//                           instanceplaceholderAttribute operator value) ||
+//                           instanceplaceholderAttribute operator value
+
+//what is happening with the following statement
+//the collection workers is passed into the .Where method
+//the .Where method does it's thing
+//the .Where method will return a temporary collection in method
+//the returned collection is then passed into the .ToList method
+//the .ToList converts the collection into a List<T> dataset
+//the converted dataset is return from List<T>
+//the dataset is assigned to the receiving variable
+var oldWorkers = workers.Where(x => x.Age > 50).ToList(); //example of Linq method syntax
+DisplayCollection(oldWorkers);
+
+// --- STEP 2: Order  -------------------------------
+//OrderBy(predicate) is ascending, use on first ordering field
+//OrderByDescending(predicate) is descending, use on first ordering field
+//ThenBy(predcate) is ascending, use for second, third, .... ordering field
+//ThenByDescending(predicate) is descending, use for second, third, .... ordering field
+
+//var YoungWorkers = workers.Where(x => x.Age < 51);
+//var sortedYoungWorkers = YoungWorkers.OrderBy(x => x.Age);
+
+//combine these statements into one
+
+// the workers collection is past into .Where
+//.Where return a resulting collection
+//.Where collection is past into .OrderBy
+//.OrderBy returns a resulting collection
+//.OrderBY collection is assigned to sortedYoungWorkers
+var sortedYoungWorkers = workers.Where(x => x.Age < 51)
+                                .OrderBy(x => x.Age)
+                                .ThenByDescending(x => x.Wage)
+                                .ToList();
+DisplayCollection(sortedYoungWorkers);
+
+// --- STEP 3: Finding items in a Collection  -------------------------------
+
+//Using the .Where(...) to locate an item
+//Where(  ) returns a collection. That collection maybe empty, one record, or more many records
+//see Where examples above
+
+//What if you just wish to know if an item is in the collection?
+//You does not need to want a returned collection
+//The are two boolean filter commands for searching your collection: Any or All
+//Any(predicate) will return true if any instance in your collection matching the predicate : return is a boolean
+//All(predicate) will return true if all instances in your collection matches the predicate : return is a boolean
+
+//These are very useful in decision making (if(... ) statement)
+if (workers.All(x => x.Age <= 65))
+    // all instances match the condition within the predicate
+    Console.WriteLine("\nThe workers all need to pay into CPP.");
+else
+    // at least one instances does not match the condition within the predicate
+    Console.WriteLine("\nSome workers do not need to pay into CPP.");
+
+if (workers.Any(x => x.Age > 65))
+{
+    // at least one instances matches the condition within the predicate
+    Console.WriteLine($"\nAt least one worker does not need to pay into CPP.");
+}
+else
+{
+    // no instances match the condition within the predicate
+    Console.WriteLine($"\nThe workers all need to pay into CPP. No old people.");
+}
+
+// --- STEP 4: Finding items in a Collection without using .Where ----------------------
+//What if you wish to locate an instance in your collection by a unique key value (pkey)
+//The solution will possible return an instance or null
+//possible solutions:
+// You can use Where(...)
+// You can also use collection methods such as Find(..), FindAll(...), FindLast(...), FindIndex (...)
+// You could also use a Linq method such as First<>([...]) or FirstOrDefault<>([...])
+Person foundThem = workers.Find(x => x.Age > 29 &&  x.Age < 60);
+
+if (foundThem != null)
+    Console.WriteLine($"\nFound {foundThem.Name} is of age {foundThem.Age}.");
+else
+    Console.WriteLine($"\nNo workers in that age bracket.");
+
+//CANNOT use Linq method with collection methods
+//OrderBy is a Linq method
+//Find is a collection method
+//Person OrderfoundThem = workers.OrderBy(x => x.Age).Find(x => x.Age > 19 &&  x.Age < 60);
+
+//Linq method
+Person LinqfoundThem = workers.FirstOrDefault(x => x.Age > 19 &&  x.Age < 50);
+if (LinqfoundThem != null)
+    Console.WriteLine($"\nFound {LinqfoundThem.Name} is of age {LinqfoundThem.Age}.");
+else
+    Console.WriteLine($"\nNo workers in that age bracket.");
+
+Person OrderLinqfoundThem = workers.OrderBy(x => x.Age)
+                                    .FirstOrDefault(x => x.Age > 19 &&  x.Age < 50);
+
+if (OrderLinqfoundThem != null)
+    Console.WriteLine($"\nFound {OrderLinqfoundThem.Name} is of age {OrderLinqfoundThem.Age}.");
+else
+    Console.WriteLine($"\nNo workers in that age bracket.");
+
+/* ************************* methods ********************************* */
+
+//Remember to isolate your methods use : static
+//variables are now local and NOT global
+//WARNING! List is a reference variable type
+//          any changes to list will still be present when
+//              you return to your driver code
+static void DisplayCollection(List<Person> workers)
+    {
+        //foreach loop will process your collection from the 1st item to the last item
+        //foreach is a While loop structure (pre-test loop)
+        //"item" placeholder only exists as long as the foreach is executing
+        Console.WriteLine($"\nWorker list Collection.\n");
+        foreach (var item in workers)
+        {
+            Console.WriteLine($"The item in the collection is: {item.ToString()}");
+        }
+    }
