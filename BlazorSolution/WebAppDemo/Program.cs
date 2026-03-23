f48750bcce8fd814bf7e91c094f95fe340c7727b
Don Welch <dwelch@nait.ca>
2026-03-23 14:42:10 -0600

    complete DB setup and registration of context and services in Program.cs and demo use in lesson2425.raxor

--- BlazorSolution/WebAppDemo/Program.cs
+++ BlazorSolution/WebAppDemo/Program.cs
@@ -1,6 +1,36 @@
 using WebAppDemo.Components;
 
+#region Additional Namespaces
+using Microsoft.EntityFrameworkCore;
+using WestWindSystem.DAL;
+using WestWindSystem.BLL;
+#endregion
+
 var builder = WebApplication.CreateBuilder(args);
+
+// obtain the connection to supply to out DAL context class
+// We will be registering the db connection AND our services
+//      we create within the BLL folder of your library
+
+//grab your connection string from its declared location
+// either appsettings.json
+//     or user secrets  
+
+//access is first to appsettings.json
+//then to user secrets
+//if there is a declaration in both
+// the declaration in appsettings is over ridden with the
+//          declaration in user secrets.
+var connectstring = builder.Configuration.GetConnectionString("WWDB");
+
+// register the DbContext to the Sql Server
+builder.Services.AddDbContext<WestWindContext>(options  => options.UseSqlServer(connectstring));
+
+//Register your service class
+//One for EACH service class in your BLL
+//project recommendation, ADD lastname to the front of your service class
+builder.Services.AddScoped<ProductServices>();
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
---
+++ BlazorSolution/WestWindSystem/BLL/ProductServices.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#region Additional Namespaces
+using WestWindSystem.DAL;
+using WestWindSystem.Models;
+#endregion
+
+namespace WestWindSystem.BLL
+{
+    public class ProductServices
+    {
+        #region setup constructor and private local context variable
+        private readonly WestWindContext _context;
+
+        public ProductServices(WestWindContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Queries
+        public async Task<List<Product>> Product_GetAll()
+        {
+            return await _context.Products
+                                .Include(x => x.Category)
+                                .Include(x => x.Supplier)
+                                .OrderBy(x => x.ProductName)
+                                .ToListAsync();
+        }
+        #endregion
+    }
+}
