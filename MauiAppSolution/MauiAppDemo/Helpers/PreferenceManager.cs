00ee492a5ff4229bc2ad86f8e640223dbaa29086
Don Welch <dwelch@nait.ca>
2026-04-09 16:54:00 -0600

    settings demo

---
+++ MauiAppSolution/MauiAppDemo/Helpers/PreferenceManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiAppDemo.Helpers
+{
+    public static class PreferenceManager
+    {
+        public static event Action? OnPreferenceChanged;
+
+        public static void NotifyPreferenceChanged()
+        {
+            OnPreferenceChanged?.Invoke();
+        }
+    }
+}
